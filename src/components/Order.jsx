fc59145aa9188b8c5b8e724f5e02cae7be2f640c
nomadriding <fundora.a@gmail.com>
2025-04-12 13:50:10 -0400

    order page

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -1,5 +1,12 @@
 import React from "react"
 
-function Order() {}
+function Order() {
+  return (
+    <div>
+      <h1>Order Page</h1>
+      <p>Welcome to the Order page!</p>
+    </div>
+  )
+}
 
 export default Order
