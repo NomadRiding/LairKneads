b95b364cb23d51a19e27e51118674dc5c4004ef0
nomadriding <fundora.a@gmail.com>
2025-04-13 22:45:31 -0400

    adjust the reviews

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -5,81 +5,105 @@ function Review() {
   return (
     <div className="review-page">
       <div className="reviewSquare">
-        <div className="profile">
-          <img
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIEA2_vTW1kjFz5W0otnsEBJrXXMI5SYi2KQ&s"
-            alt="Profile"
-            className="profile-image"
-          />
-          <div className="profile-details">
-            <h2 className="profile-name">Betty M.</h2>
-            <div className="starRating">
-              <link
-                rel="stylesheet"
-                href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-              />
-              <span className="fa fa-star checked"></span>
-              <span className="fa fa-star checked"></span>
-              <span className="fa fa-star checked"></span>
-              <span className="fa fa-star checked"></span>
-              <span className="fa fa-star checked"></span>
+        <div className="headerSpace">
+          <div className="profile">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIEA2_vTW1kjFz5W0otnsEBJrXXMI5SYi2KQ&s"
+              alt="Profile"
+              className="profile-image"
+            />
+            <div className="profile-details">
+              <h2 className="profile-name">Alyssa R.</h2>
+              <div className="starRating">
+                <link
+                  rel="stylesheet"
+                  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+                />
+                <span className="fa fa-star checked"></span>
+                <span className="fa fa-star checked"></span>
+                <span className="fa fa-star checked"></span>
+                <span className="fa fa-star checked"></span>
+                <span className="fa fa-star checked"></span>
+              </div>
             </div>
           </div>
         </div>
+        <p className="reviewTitle">
+          <strong>Chocolate Craving Crusher</strong>
+        </p>
         <div className="reviewText">
-          <p className="review-text">This is a review text.</p>
+          <p className="review-text">
+            The Chocolate banana bread is dangerously good. Rich, soft, and gone
+            in one sitting.
+          </p>
         </div>
       </div>
       <div className="reviewSquare">
-        <div className="profile">
-          <img
-            src="https://www.brandtalent.net/images/models/6261/ritzsport.jpg"
-            alt="Profile"
-            className="profile-image"
-          />
-          <div className="profile-details">
-            <h2 className="profile-name">Jason L.</h2>
-            <div className="starRating">
-              <link
-                rel="stylesheet"
-                href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-              />
-              <span className="fa fa-star checked"></span>
-              <span className="fa fa-star checked"></span>
-              <span className="fa fa-star checked"></span>
-              <span className="fa fa-star checked"></span>
-              <span className="fa fa-star checked"></span>
+        <div className="headerSpace">
+          <div className="profile">
+            <img
+              src="https://barbend.com/wp-content/uploads/2024/01/Wells.jpg"
+              alt="Profile"
+              className="profile-image"
+            />
+            <div className="profile-details">
+              <h2 className="profile-name">Camille D.</h2>
+              <div className="starRating">
+                <link
+                  rel="stylesheet"
+                  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+                />
+                <span className="fa fa-star checked"></span>
+                <span className="fa fa-star checked"></span>
+                <span className="fa fa-star checked"></span>
+                <span className="fa fa-star checked"></span>
+                <span className="fa fa-star checked"></span>
+              </div>
             </div>
           </div>
         </div>
+        <p className="reviewTitle">
+          <strong>One more bite...</strong>
+        </p>
         <div className="reviewText">
-          <p className="review-text">This is a review text.</p>
+          <p className="review-text">
+            Can't believe how delicious these are. I tried a slice, and now I
+            order every weekend!
+          </p>
         </div>
       </div>
       <div className="reviewSquare">
-        <div className="profile">
-          <img
-            src="https://media.istockphoto.com/id/1335941248/photo/shot-of-a-handsome-young-man-standing-against-a-grey-background.jpg?s=612x612&w=0&k=20&c=JSBpwVFm8vz23PZ44Rjn728NwmMtBa_DYL7qxrEWr38="
-            alt="Profile"
-            className="profile-image"
-          />
-          <div className="profile-details">
-            <h2 className="profile-name">Rob S.</h2>
-            <div className="starRating">
-              <link
-                rel="stylesheet"
-                href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-              />
-              <span className="fa fa-star checked"></span>
-              <span className="fa fa-star checked"></span>
-              <span className="fa fa-star checked"></span>
-              <span className="fa fa-star checked"></span>
-              <span className="fa fa-star checked"></span>
+        <div className="headerSpace">
+          <div className="profile">
+            <img
+              src="https://media.istockphoto.com/id/1335941248/photo/shot-of-a-handsome-young-man-standing-against-a-grey-background.jpg?s=612x612&w=0&k=20&c=JSBpwVFm8vz23PZ44Rjn728NwmMtBa_DYL7qxrEWr38="
+              alt="Profile"
+              className="profile-image"
+            />
+            <div className="profile-details">
+              <h2 className="profile-name">Marcus J.</h2>
+              <div className="starRating">
+                <link
+                  rel="stylesheet"
+                  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+                />
+                <span className="fa fa-star checked"></span>
+                <span className="fa fa-star checked"></span>
+                <span className="fa fa-star checked"></span>
+                <span className="fa fa-star checked"></span>
+                <span className="fa fa-star checked"></span>
+              </div>
             </div>
           </div>
         </div>
+        <p className="reviewTitle">
+          <strong>Spicy Surprise</strong>
+        </p>
         <div className="reviewText">
-          <p className="review-text">This is a review text.</p>
+          <p className="review-text">
+            Didn't expect to love Jalapeno banana bread, but wow! <br /> it's my
+            new fav combo. Perfect sweet heat.
+          </p>
         </div>
       </div>
     </div>
