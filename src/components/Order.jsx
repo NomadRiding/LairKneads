1c0a42e243416cde3d4c3710f63a8a3a26f79db0
nomadriding <fundora.a@gmail.com>
2025-04-13 22:45:18 -0400

    modify css for order

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -5,17 +5,17 @@ import "./Order.css" // Import the CSS file for styling
 function Order() {
   return (
     <Layout>
-      <h1 style={{ textAlign: "center", marginTop: "70px" }}>
-        Banana Bread Order Form
-      </h1>
-      <p className="Order-Details">
-        Please fill out this form, you'll receive a Text with confirmation and
-        final calculated cost; once confirmed you'll be added to the list.
-        Batches are made every Friday/Saturday, and
-        <span>Delivery is every Saturday.</span>
-        Payments are through Zelle or Cash, and are due at the time of delivery.
-      </p>
-      {/* <div className="OrderForm">
+      <div className="order-container">
+        <h1>Banana Bread Order Form</h1>
+        <p className="Order-Details">
+          Please fill out this form, you'll receive a Text with confirmation and
+          final calculated cost; once confirmed you'll be added to the list.
+          Batches are made every Friday/Saturday, and
+          <span>Delivery is every Saturday.</span>
+          Payments are through Zelle or Cash, and are due at the time of
+          delivery.
+        </p>
+        {/* <div className="OrderForm">
         <form
           action="https://docs.google.com/forms/d/e/1FAIpQLSdMlDWH9U64FMpd3fk79veBhdIa6jMeUK0LV9G6yKUzfzgGRQ/formResponse"
           method="POST"
@@ -51,22 +51,23 @@ function Order() {
           <button type="submit">Submit</button>
         </form>
       </div> */}
-      <iframe
-        src="https://docs.google.com/forms/d/e/1FAIpQLSdMlDWH9U64FMpd3fk79veBhdIa6jMeUK0LV9G6yKUzfzgGRQ/viewform?embedded=true"
-        width="640"
-        height="1700"
-        frameborder="0"
-        marginheight="0"
-        marginwidth="0"
-        style={{
-          display: "block",
-          margin: "0 auto",
-          border: "none",
-          overflow: "hidden",
-        }}
-      >
-        Loading…
-      </iframe>
+        <iframe
+          src="https://docs.google.com/forms/d/e/1FAIpQLSdMlDWH9U64FMpd3fk79veBhdIa6jMeUK0LV9G6yKUzfzgGRQ/viewform?embedded=true"
+          width="640"
+          height="1700"
+          frameborder="0"
+          marginheight="0"
+          marginwidth="0"
+          style={{
+            display: "block",
+            margin: "0 auto",
+            border: "none",
+            overflow: "hidden",
+          }}
+        >
+          Loading…
+        </iframe>
+      </div>
     </Layout>
   )
 }
