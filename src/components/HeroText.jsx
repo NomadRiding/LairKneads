ca17f0e3b4de9f56c92ea22a533e137b5ca9d422
nomadriding <fundora.a@gmail.com>
2025-04-15 22:23:19 -0400

    change css to more playful look on site

--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -15,9 +15,9 @@ function HeroText() {
         <h1>
           Welcome to <span className="Logo-style">Lair Kneads</span>
         </h1>
-        <p>Your one-stop shop for delicious baked goods!</p>
+        <p>Fresh. Weekly Drops. Gone Fast!</p>
       </div>
-      <button onClick={handleOrderClick}>Order</button> {/* Add onClick */}
+      <button onClick={handleOrderClick}>Order Now</button>
     </div>
   )
 }
