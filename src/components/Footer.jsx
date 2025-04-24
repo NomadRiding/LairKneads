80e2e1f365688657ce7ef58af61f57fc12224a46
nomadriding <fundora.a@gmail.com>
2025-04-23 22:28:37 -0400

    update footer added blog link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,38 +7,51 @@ import Tiktok from "../assets/TiktokBlack.png"
 function Footer() {
   return (
     <footer className="footer">
-      <div className="footer-content">
+      <div className="footerBody">
         <p>&copy; 2025 Lair Kneads. All rights reserved.</p>
-        <p>Follow us on:</p>
-        <ul className="social-media-links">
-          <li>
-            <a
-              href="https://www.instagram.com/lair_kneads"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img src={Instagram} alt="Instagram" />
+        <div className="footer-content">
+          <div className="notable-links">
+            <a href="/Recipes" className="linkable-item">
+              Recipes
             </a>
-          </li>
-          <li>
-            <a
-              href="https://www.twitter.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img src={XLogo} alt="Twitter" />
+            <a href="/Blog" className="linkable-item">
+              Blog
             </a>
-          </li>
-          <li>
-            <a
-              href="https://www.tiktok.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img src={Tiktok} alt="TikTok" />
-            </a>
-          </li>
-        </ul>
+          </div>
+          <div className="FollowSection">
+            <p>Follow us on:</p>
+            <ul className="social-media-links">
+              <li>
+                <a
+                  href="https://www.instagram.com/lair_kneads"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={Instagram} alt="Instagram" />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.twitter.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={XLogo} alt="Twitter" />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.tiktok.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={Tiktok} alt="TikTok" />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
         <p className="disclaimer">
           *Lair Kneads is a Cottage Food Operation. All products are homemade
           and not subject to routine government food safety inspections.
@@ -53,9 +66,6 @@ function Footer() {
         </a>
         <a href="/contact-us" className="linkable-item">
           Contact Us
-        </a>
-        <a href="/Recipes" className="linkable-item">
-          Recipes
         </a>
       </div>
     </footer>
