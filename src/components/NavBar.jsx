6fef44cb6d0673d36eb192a4e56d6e58048ea653
nomadriding <fundora.a@gmail.com>
2025-05-04 01:46:17 -0400

    add a banner

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,35 +1,30 @@
 import React from "react"
-import { useEffect, useState } from "react"
 import "./NavBar.css"
-import { Link } from "react-router-dom"
-import Order from "./Order"
 
 function NavBar() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
   return (
-    <nav>
-      <h1 className="Logo-style logo">
-        <Link to="/">Lair Kneads</Link>
-      </h1>
-      <ul>
-        <li>
-          <Link to="/Order">Order</Link>
-        </li>
-        <li>
-          <Link to="/About">About</Link>
-        </li>
-        <li>
-          <Link to="/Contact">Contact</Link>
-        </li>
-        {/* <li>
-          {isLoggedIn ? (
-            <Link to="/profile">Profile</Link> // Show "Profile" if logged in
-          ) : (
-            <Link to="/login">LogIn</Link> // Show "LogIn" if not logged in
-          )}
-        </li> */}
-      </ul>
-    </nav>
+    <>
+      <div className="NavBanner">
+        <p>We are Veteran Owned and Operated!</p>
+      </div>
+      <nav>
+        <div className="logo">Lair Kneads</div>
+        <ul>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#products">Products</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </>
   )
 }
 
